11d18c95fe3bffe5810149e61b7d3c6c92de5e52
Nirup <nirup.ch@gmail.com>
2020-05-13 15:48:18 -0400

    sample test2

--- src/main/java/test/Sample.java
+++ src/main/java/test/Sample.java
@@ -3,6 +3,6 @@ package testmvn;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello sample world and welcome to all ");
+		System.out.println("Hello sample world & welcome to all.");
 	}
 }
